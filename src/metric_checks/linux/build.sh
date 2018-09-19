ec14458995a795d88dbf850c98121891627e1d98
Jef Spaleta <jef@sensu.io>
2018-09-19 15:55:54 -0800

    check for nested library deps among libraries

--- src/metric_checks/linux/build.sh
+++ src/metric_checks/linux/build.sh
@@ -3,9 +3,16 @@
 mkdir -p ./build/bin
 mkdir -p ./build/lib
 
-array=( "duh" "mpstat" "bash" "iostat" "df" "awk" "free" )
+scripts=( "metrics-nagios.sh" )
+
+for script in "${scripts[@]}"; do
+  echo "shell-scripts/${script}"
+  cp -n "shell-scripts/${script}" build/bin
+done
+
+commands=( "duh" "mpstat" "bash" "iostat" "df" "awk" "free" )
 all_libs=()
-for c in "${array[@]}"; do
+for c in "${commands[@]}"; do
   full_path=`which $c`
   if [ ! -z $full_path ] ; then
     echo $c $full_path
@@ -14,9 +21,33 @@ for c in "${array[@]}"; do
     cp -n $full_path build/bin/
   fi
 done
+sorted_unique_libs=($(echo "${all_libs[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 
-for l in "${all_libs[@]}"; do
+for l in "${sorted_unique_libs[@]}"; do
   cp -n $l build/lib/
 done
+saved_libs=( $(ls -1 build/lib/ | sort -u) )
 
-# ldd $(which mpstat)  | grep "=> /" | awk '{print $3}' | xargs -I '{}' cp -v '{}' /
+echo ${#saved_libs[@]}  ${#sorted_unique_libs[@]}
+
+all_libs=()
+for lib in "${saved_libs[@]}"; do
+  full_path="build/lib/${lib}"
+  if [ ! -z $full_path ] ; then
+    echo $lib $full_path
+    libs=`ldd $full_path | grep "=> /" | awk '{print $3}'`
+    all_libs+=( "${libs[@]}" )
+    cp -n $full_path build/bin/
+  fi
+done
+sorted_unique_libs=($(echo "${all_libs[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+new_saved_libs=( $(ls -1 build/lib/ | sort -u) )
+echo ${#saved_libs[@]}  ${#new_saved_libs[@]}
+
+if (( ${#new_saved_libs[@]} <= ${#saved_libs[@]} )) ; then
+  echo "no new libs"
+  process="false"
+else
+  echo "new libs added re-run build.sh to make sure deps are added" 
+fi
+
